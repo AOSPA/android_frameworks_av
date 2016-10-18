499773ab0dee9faca7a4112529ae23e463e8005f
Christopher N. Hesse <raymanfx@gmail.com>
2016-10-18 20:56:33 +0200

    libstagefright: Support for legacy camera/encoder buffers
    Since Android 7.0, the default way of passing buffers
    between camera HAL, encoder and framework is via
    Android Native Window (ANW) buffers.
    
    Pre 7.0 devices did this using buffer_handle_t buffers
    allocated by gralloc HAL.
    Since many devices do not have the source code for their
    camera HALs or the encoder libraries available, introduce
    this hack.
    
    Change-Id: I202051ab5d7273a33be0aa32b0bfdbbb1f53693f
    Signed-off-by: Ícaro Hoff <icarohoff@gmail.com>

--- media/libstagefright/Android.mk
+++ media/libstagefright/Android.mk
@@ -149,6 +149,10 @@ ifeq ($(BOARD_USE_SAMSUNG_CAMERAFORMAT_NV21), true)
 LOCAL_CFLAGS += -DUSE_SAMSUNG_CAMERAFORMAT_NV21
 endif
 
+ifeq ($(TARGET_HAS_LEGACY_CAMERA_HAL1),true)
+LOCAL_CFLAGS += -DCAMCORDER_GRALLOC_SOURCE
+endif
+
 LOCAL_CFLAGS += -Wno-multichar -Werror -Wno-error=deprecated-declarations -Wall
 
 # enable experiments only if requested
