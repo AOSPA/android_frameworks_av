32b2440d6a54b928399fc64c127fedcd7e326dfc
Eino-Ville Talvala <etalvala@google.com>
2016-12-08 11:20:51 -0800

    Camera service: Speed up camera service startup on boot
    In the common case, cameraserver starts before system server, which
    contains the proxy camera service.  This means that
    getCameraServiceProxy() will wait for the proxy to be up (or time out
    after 5 seconds).  Since the proxy will call the camera service once it
    is up and running, there's no reason to block.
    
    Switch to checkService() instead of getService() to remove this timeout
    and fail immediately.
    
    Test: Manual: Build and boot phone; confirm that camera functions normally
    Change-Id: I03596c18b13c68b5bcb15ee71de465cd14fba4de

--- services/camera/libcameraservice/CameraService.cpp
+++ services/camera/libcameraservice/CameraService.cpp
@@ -273,7 +273,10 @@ void CameraService::onFirstRef()
 
 sp<ICameraServiceProxy> CameraService::getCameraServiceProxy() {
     sp<IServiceManager> sm = defaultServiceManager();
-    sp<IBinder> binder = sm->getService(String16("media.camera.proxy"));
+    // Use checkService because cameraserver normally starts before the
+    // system server and the proxy service. So the long timeout that getService
+    // has before giving up is inappropriate.
+    sp<IBinder> binder = sm->checkService(String16("media.camera.proxy"));
     if (binder == nullptr) {
         return nullptr;
     }
