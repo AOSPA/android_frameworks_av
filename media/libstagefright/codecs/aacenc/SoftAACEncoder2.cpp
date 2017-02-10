a88acf55fe9a2740206590e856287536e5471c16
Wonsik Kim <wonsik@google.com>
2017-02-10 14:29:40 +0900

    codecs: handle onReset() for a few encoders
    Test: Run PoC binaries
    Bug: 34749392
    Bug: 34705519
    Change-Id: I3356eb615b0e79272d71d72578d363671038c6dd
    (cherry picked from commit 5443b57cc54f2e46b35246637be26a69e9f493e1)
    (cherry picked from commit bb1408b3969ffb097e3bbdf241ccd84de6448bfb)

--- media/libstagefright/codecs/aacenc/SoftAACEncoder2.cpp
+++ media/libstagefright/codecs/aacenc/SoftAACEncoder2.cpp
@@ -72,8 +72,7 @@ SoftAACEncoder2::SoftAACEncoder2(
 SoftAACEncoder2::~SoftAACEncoder2() {
     aacEncClose(&mAACEncoder);
 
-    delete[] mInputFrame;
-    mInputFrame = NULL;
+    onReset();
 }
 
 void SoftAACEncoder2::initPorts() {
@@ -710,6 +709,17 @@ void SoftAACEncoder2::onQueueFilled(OMX_U32 /* portIndex */) {
 
         mInputSize = 0;
     }
+}
+
+void SoftAACEncoder2::onReset() {
+    delete[] mInputFrame;
+    mInputFrame = NULL;
+    mInputSize = 0;
+
+    mSentCodecSpecificData = false;
+    mInputTimeUs = -1ll;
+    mSawInputEOS = false;
+    mSignalledError = false;
 }
 
 }  // namespace android
