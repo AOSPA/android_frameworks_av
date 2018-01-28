0720874811759d229faa5367e463357f33c0ceff
Rashed Abdel-Tawab <rashed@linux.com>
2018-01-28 11:41:17 -0800

    camera: Only link and use vendor.qti.hardware.camera.device if specified
    Set TARGET_USES_QTI_CAMERA_DEVICE to link and use
    
    Change-Id: I3a82412190b8918248bdd7a1d823777debd67173
    Signed-off-by: Akhil Narang <akhilnarang.1999@gmail.com>

--- camera/cameraserver/Android.mk
+++ camera/cameraserver/Android.mk
@@ -30,9 +30,14 @@ LOCAL_SHARED_LIBRARIES := \
 	android.hardware.camera.common@1.0 \
 	android.hardware.camera.provider@2.4 \
 	android.hardware.camera.device@1.0 \
-	vendor.qti.hardware.camera.device@1.0 \
 	android.hardware.camera.device@3.2 \
 	android.hidl.manager@1.0
+
+ifeq ($(TARGET_USES_QTI_CAMERA_DEVICE), true)
+LOCAL_CFLAGS += -DQTI_CAMERA_DEVICE
+LOCAL_SHARED_LIBRARIES += \
+	vendor.qti.hardware.camera.device@1.0
+endif
 
 LOCAL_MODULE:= cameraserver
 LOCAL_32_BIT_ONLY := true
