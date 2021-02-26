065c2bc2006da73182f0329772dc1516767c4983
Aniket Kumar Lata <alata@codeaurora.org>
2021-02-25 16:46:56 -0800

    media: Set AAC profile key for CCodec based on encoder mode
    StagefrightRecorder sets "aac-profile" key with OMX specific
    value for ACodec based on audio encoder mode from the app.
    This information is required by CCodec as well in a form that
    can be interpreted by it i.e. with the right key/value pair.
    Set "profile" key for CCodec to map from MediaCodecConstants
    to C2Config::profile_t.
    
    CRs-Fixed: 2889259
    Change-Id: I99e0607daffeb345278d1b0340edc57b6a6a04cb

--- media/libmediaplayerservice/StagefrightRecorder.cpp
+++ media/libmediaplayerservice/StagefrightRecorder.cpp
@@ -53,6 +53,7 @@
 #include <media/stagefright/MediaDefs.h>
 #include <media/stagefright/MetaData.h>
 #include <media/stagefright/MediaCodecSource.h>
+#include <media/stagefright/MediaCodecConstants.h>
 #include <media/stagefright/OggWriter.h>
 #include <media/stagefright/PersistentSurface.h>
 #include <media/MediaProfiles.h>
@@ -1385,14 +1386,17 @@ sp<MediaCodecSource> StagefrightRecorder::createAudioSource() {
         case AUDIO_ENCODER_AAC:
             format->setString("mime", MEDIA_MIMETYPE_AUDIO_AAC);
             format->setInt32("aac-profile", OMX_AUDIO_AACObjectLC);
+            format->setInt32("profile", AACObjectLC);
             break;
         case AUDIO_ENCODER_HE_AAC:
             format->setString("mime", MEDIA_MIMETYPE_AUDIO_AAC);
             format->setInt32("aac-profile", OMX_AUDIO_AACObjectHE);
+            format->setInt32("profile", AACObjectHE);
             break;
         case AUDIO_ENCODER_AAC_ELD:
             format->setString("mime", MEDIA_MIMETYPE_AUDIO_AAC);
             format->setInt32("aac-profile", OMX_AUDIO_AACObjectELD);
+            format->setInt32("profile", AACObjectELD);
             break;
         case AUDIO_ENCODER_OPUS:
             format->setString("mime", MEDIA_MIMETYPE_AUDIO_OPUS);
