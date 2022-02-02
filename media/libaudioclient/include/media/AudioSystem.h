4749c327aafed0581f95b76d164f9daf1987b79a
cjybyjk <cjybyjk@zjnu.edu.cn>
2022-02-02 15:34:03 +0800

    av: Add support per-app volume [1/3]
    Change-Id: Ib22a04d7b99706042b903a223d0b3900c0838fbd
    Signed-off-by: cjybyjk <cjybyjk@zjnu.edu.cn>
    Signed-off-by: chrisw444 <wandersonrodriguesf1@gmail.com>
    Signed-off-by: chrisl7 <wandersonrodriguesf1@gmail.com>

--- media/libaudioclient/include/media/AudioSystem.h
+++ media/libaudioclient/include/media/AudioSystem.h
@@ -33,6 +33,7 @@
 #include <android/media/audio/common/AudioPort.h>
 #include <media/AidlConversionUtil.h>
 #include <media/AudioContainers.h>
+#include <media/AppVolume.h>
 #include <media/AudioDeviceTypeAddr.h>
 #include <media/AudioPolicy.h>
 #include <media/AudioProductStrategy.h>
@@ -648,6 +649,10 @@ public:
 
     static status_t setVibratorInfos(const std::vector<media::AudioVibratorInfo>& vibratorInfos);
 
+    static status_t setAppVolume(const String8& packageName, const float value);
+    static status_t setAppMute(const String8& packageName, const bool value);
+    static status_t listAppVolumes(std::vector<media::AppVolume> *vols);
+    
     static status_t getMmapPolicyInfo(
             media::audio::common::AudioMMapPolicyType policyType,
             std::vector<media::audio::common::AudioMMapPolicyInfo> *policyInfos);
