a6dec538494714374e32d60868bacfe519ec6bc9
cjh1249131356 <cjh1249131356@gmail.com>
2022-07-22 22:55:18 +0800

    libcameraservice: Add support to set vendor tag with client package name
    - OEMs like OnePlus and Nothing detect camera package name to unlock features like 48mp.
    
    [PA Edit: Make this generic by defining the package
     name rather than the OEM]
    
    Change-Id: Ic0b537820ef1874647911379bcf142f0768a7a76
    Co-Authored-By: Jake Weinstein <jake@aospa.co>
    Signed-off-by: cjh1249131356 <cjh1249131356@gmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- services/camera/libcameraservice/CameraService.h
+++ services/camera/libcameraservice/CameraService.h
@@ -350,6 +350,8 @@ public:
      */
     bool isAutomotiveExteriorSystemCamera(const std::string& cameraId) const;
 
+    static std::string getCurrPackageName();
+
     /////////////////////////////////////////////////////////////////////
     // CameraClient functionality
 
