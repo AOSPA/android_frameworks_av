f09685529a5f9c53a6760e1e8ff83ed1ee74f9d0
cjh1249131356 <cjh1249131356@gmail.com>
2022-07-22 22:55:18 +0800

    libcameraservice: Add support to set vendor tag with client package name
    * OEMs like OnePlus and Nothing detect camera package name to unlock features like 48mp.
    
    [PA Edit: Make this generic by defining the package
     name rather than the OEM]
    
    Change-Id: Ic0b537820ef1874647911379bcf142f0768a7a76
    Co-Authored-By: Jake Weinstein <jake@aospa.co>
    Signed-off-by: cjh1249131356 <cjh1249131356@gmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: chrisl7 <wandersonrodriguesf1@gmail.com>

--- services/camera/libcameraservice/CameraService.cpp
+++ services/camera/libcameraservice/CameraService.cpp
@@ -171,6 +171,9 @@ constexpr int32_t kInvalidDeviceId = -1;
 // Set to keep track of logged service error events.
 static std::set<std::string> sServiceErrorEventSet;
 
+// Current camera package name
+static std::string sCurrPackageName;
+
 CameraService::CameraService(
         std::shared_ptr<CameraServiceProxyWrapper> cameraServiceProxyWrapper,
         std::shared_ptr<AttributionAndPermissionUtils> attributionAndPermissionUtils) :
@@ -1467,6 +1470,10 @@ Status CameraService::filterGetInfoErrorCode(status_t err) {
     }
 }
 
+std::string CameraService::getCurrPackageName() {
+    return sCurrPackageName;
+}
+
 Status CameraService::makeClient(const sp<CameraService>& cameraService,
         const sp<IInterface>& cameraCb, const std::string& packageName, bool systemNativeClient,
         const std::optional<std::string>& featureId,  const std::string& cameraId,
@@ -2442,6 +2449,8 @@ Status CameraService::connectHelper(const sp<CALLBACK>& cameraCb, const std::str
     ALOGI("CameraService::connect call (PID %d \"%s\", camera ID %s) and "
             "Camera API version %d", packagePid, clientPackageName.c_str(), cameraId.c_str(),
             static_cast<int>(effectiveApiLevel));
+
+    sCurrPackageName = clientName8.string();
 
     nsecs_t openTimeNs = systemTime();
 
