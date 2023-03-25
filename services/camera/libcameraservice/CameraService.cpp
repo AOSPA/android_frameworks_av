a09b50b8985b129887a21f48f4ba36c0d87aee4c
Lostark13 <tushar9915@gmail.com>
2023-03-25 15:53:40 +0000

    cameraserver: Let multiple cameras opened by stock camera application
    Let conflicting camera devices list be empty and let the camera hal manage it.
    Old camera hals mark camera device as conflicting if the new camera device is opened.
    Fixes dual video camera mode for old devices
    
    Change-Id: I47a4e05dd0bfb76e3ebcebbfc1d173dc0de5cdba
    Signed-off-by: Lostark13 <tushar9915@gmail.com>

--- services/camera/libcameraservice/CameraService.cpp
+++ services/camera/libcameraservice/CameraService.cpp
@@ -1374,6 +1374,10 @@ void CameraService::finishConnectLocked(const sp<BasicClient>& client,
                     oomScoreOffset, systemNativeClient);
     auto evicted = mActiveClientManager.addAndEvict(clientDescriptor);
 
+    if (strcmp(String8(client->getPackageName()).string(), "com.android.camera") == 0) {
+        evicted.clear();
+    }
+
     logConnected(desc->getKey(), static_cast<int>(desc->getOwnerId()),
             String8(client->getPackageName()));
 
@@ -1483,6 +1487,9 @@ status_t CameraService::handleEvictionsLocked(const String8& cameraId, int clien
         // Find clients that would be evicted
         auto evicted = mActiveClientManager.wouldEvict(clientDescriptor);
 
+        if (strcmp(String8(packageName).string(), "com.android.camera") == 0) {
+            evicted.clear();
+        }
         // If the incoming client was 'evicted,' higher priority clients have the camera in the
         // background, so we cannot do evictions
         if (std::find(evicted.begin(), evicted.end(), clientDescriptor) != evicted.end()) {
