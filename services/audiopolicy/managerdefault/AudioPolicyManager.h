42c411597e9c30a188bb870bd2a1a65a3c37e2bd
Eric Laurent <elaurent@google.com>
2024-01-15 18:11:42 +0100

    audio policy: fix call routing via audio patch
    The audio source used to control the call RX path must be tagged as
    internal so that the corresponding audio patch is not realized with a
    software audio patch because of the lack of gain controller on sink device.
    
    Voice call volume is controlled via setVoiceVolume() HAL API and not
    track volume control or device gain control
    
    CRs-Fixed:3635218
    
    Bug: 302519079
    Test: make
    Change-Id: Ie9217f1be1bf127915d6bdf42d5de0834966b9d2
    (cherry picked from commit 541a200502b3a06e17ddf1d04d69a70bdae1fe1a)

--- services/audiopolicy/managerdefault/AudioPolicyManager.h
+++ services/audiopolicy/managerdefault/AudioPolicyManager.h
@@ -340,7 +340,8 @@ public:
         virtual status_t startAudioSource(const struct audio_port_config *source,
                                           const audio_attributes_t *attributes,
                                           audio_port_handle_t *portId,
-                                          uid_t uid);
+                                          uid_t uid,
+                                          bool internal = false);
         virtual status_t stopAudioSource(audio_port_handle_t portId);
 
         virtual status_t setMasterMono(bool mono);
